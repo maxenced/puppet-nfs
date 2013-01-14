3a7352fe0971411cbc1389e4b67523f0033ead4f
Maxence Dunnewind <maxence@dunnewind.net>
2013-01-14 11:41:06 +0100

    remove duplicate service def

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,14 +1,6 @@
 class nfs::config {
 
   if $::operatingsystem == 'Debian' {
-    service { 'nfs-common':
-      ensure     => running,
-      enable     => true,
-      hasrestart => true,
-      hasstatus  => false,
-      pattern    => 'rpc.idmapd',
-    }
-
     File['/etc/default/nfs-common'] {
       notify => Service['nfs-common'],
     }
